21ca9bfd4061d0367b6d16f1cff23a50ad2c78d8
FreedomWriter <natalienetworks@gmail.com>
2020-02-09 20:15:38 -0600

    dependencies installed, server running

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const server = express();
+
+server.use(cors());
+
+server.use(express.json());
+
+server.use(helmet());
+
+server.use(morgan("dev"));
+
+server.get("/", (req, res) => {
+  res.send("<h3>Sprint Challenge</h3>");
+});
+
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ message: "Something went wrong!" });
+});
+
+module.exports = server;
